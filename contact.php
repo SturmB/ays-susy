2671d792b92e7fc340204d5b672cc704731d7906
Chris McGee <chris@chrismcgee.info>
2018-05-30 16:48:22 -0400

    # Massive Image Update and More! #
    * Updates all of the Product Line splash images to show nautical-friendly samples.
    * Updates all of the individual Product images (including for hovering) to show nautical-friendly samples.
    * Adds Google Analytics tracking.
    * Updates the order form pdf (`orderform.pdf`) to match the one used by ACS.
    * Changes the background color of the Product page back to solid black.
    * Other minor tweaks.
    
    Signed-off-by: Chris McGee <chris@chrismcgee.info>

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KC9V8S"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5HFWX"
                   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
